765eea24bcc5888ba3f82e2f2da1e1d68af22a98
artikhovich <artikhovich.by@gmail.com>
2019-07-15 18:23:52 +0300

    target button toggle

--- src/components/home/target.js
+++ src/components/home/target.js
@@ -2,6 +2,17 @@ import './target.css';
 import React,{Component} from 'react';
 
 class TargetSection extends Component{
+	constructor(props){
+		super(props)
+		this.state={
+			isActive:false
+		}
+		this.readActive = () =>{
+			this.setState({
+				isActive:!this.state.isActive
+			});
+		}
+	}
 render(){
 	return(
 	<section id="target" className="section target-section">
@@ -12,11 +23,23 @@ render(){
 				<p>Ты не знаешь  кем хочешь стать?​</p>
 				<p>Мы поможем определиться с выбором!</p>
 			</div>
-			<div className="target-button">
-				<a href=""><span>Читать про профессии</span></a>
-			</div>
+			<div className="target-button-div" >
+				<button type='button' className="target-button" onClick={this.readActive}>
+				Читать про профессии
+				</button>
+			 </div>
+				{this.getLinks()}
 		</div>
 	</section>
+		)
+	}
+	getLinks(){
+		const {isActive} = this.state;
+		if(!isActive) return null
+			return(
+		<div className="get-links">
+			<h1>Section is active</h1>
+		</div>
 		)
 	}
 }
